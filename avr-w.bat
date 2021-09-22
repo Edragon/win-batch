932cb7655191ed7156337bce36758cafa4be8eb4
Electrodragon <info@electrodragon.com>
2021-09-22 17:16:27 +0800

    avrdude

---
+++ avr-w.bat
@@ -0,0 +1,15 @@
+@echo off
+
+:: folder for avrdude
+set avd=D:\Git-code\arduino\arduino-1.8.12-windows\arduino-1.8.12\hardware\tools\avr\bin\avrdude.exe
+:: batch for conf
+set cof=-CD:\Git-code\arduino\arduino-1.8.12-windows\arduino-1.8.12\hardware\tools\avr\etc\avrdude.conf
+:: batch for bootloader
+set f=D:\Git-code\arduino\arduino-1.8.12-windows\arduino-1.8.12\hardware\arduino\avr\bootloaders\optiboot\optiboot_atmega328.hex
+
+
+set cmd=%avd% %cof% -c usbasp -p m328p -b 19200 -U flash:w:"%f%":a
+
+%cmd%
+
+cmd /k
